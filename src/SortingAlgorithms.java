c4a929ee237701873ecd73c411d628c1a38fcc1b
Pratham Rathod <prathod2602005@gmail.com>
2025-09-16 08:53:58 +0530

    Initial Commit

---
+++ src/SortingAlgorithms.java
@@ -0,0 +1,13 @@
+public class SortingAlgorithms {
+    public static void bubbleSort(int[] arr) {
+
+    }
+
+    public static void quickSort(int[] arr){
+
+    }
+
+    public static void insertionSort(int[] arr){
+
+    }
+}

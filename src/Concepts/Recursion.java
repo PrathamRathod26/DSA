cc9026c445bd011c620985225c9af10f22a9172f
Pratham Rathod <prathod2602005@gmail.com>
2025-09-22 23:54:10 +0530

    Recurssion level 2

--- src/Concepts/Recursion.java
+++ src/Concepts/Recursion.java
@@ -1,5 +1,7 @@
 package Concepts;
 
+import java.util.HashSet;
+
 public class Recursion {
     public static void towerOfHanoi(int n, String source, String helper, String destination){
         if (n==1) {
@@ -46,24 +48,121 @@ public class Recursion {
             return true;
         }
 
-        if(arr[index]<arr[index+1]){
+        if(arr[index] < arr[index+1]){
             return isSorted(arr, index+1);
         } else {
             return false;
         }
     }
 
+    public static void moveAllX(String str, int index, int count, String newString){
+        if (index == str.length()){
+            for (int i = 0; i<count; i++) {
+                newString += 'x';
+            }
+            System.out.println(newString);
+            return;
+        }
+
+        char currentChar = str.charAt(index);
+        if (currentChar == 'x'){
+            count++;
+            moveAllX(str, index+1, count, newString);
+        } else {
+            newString += currentChar;
+            moveAllX(str, index+1, count, newString);
+        }
+    }
+
+
+    public static boolean[] map = new boolean[26];
+    public static void removeDuplicate(String str, int index, String newString){
+        if(index == str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currentChar = str.charAt(index);
+        if (map[currentChar - 'a']) {
+            removeDuplicate(str, index+1, newString);
+        } else {
+            newString += currentChar;
+            map[currentChar-'a'] = true;
+            removeDuplicate(str, index+1, newString);
+        }
+    }
+
+    public static void subsequences(String str, int index, String newString){
+        if(index == str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currentChar = str.charAt(index);
+
+        subsequences(str, index+1, newString+currentChar);
+
+        subsequences(str, index+1, newString);
+    }
+
+    public static void uniqueSubsequences(String str, int index, String newString, HashSet<String> set){
+        if(index == str.length()){
+            if(set.contains(newString)){
+                return;
+            } else {
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+
+        char currentChar = str.charAt(index);
+
+        uniqueSubsequences(str, index+1, newString+currentChar, set);
+
+        uniqueSubsequences(str, index+1, newString, set);
+    }
+
+    public static String[] keypad = {",", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tu", "vwx", "yz"};
+
+    public static void printCombinations(String str, int index, String combination){
+        if (index == str.length()) {
+            System.out.println(combination);
+            return;
+        }
+        char currentChar = str.charAt(index);
+        String mapping = keypad[currentChar-'0'];
+
+        for (int i = 0; i<mapping.length(); i++){
+            printCombinations(str, index+1, combination+mapping.charAt(i));
+        }
+    }
+
     public static void main(String[] args) {
         int n = 3;
-        towerOfHanoi(n, "S", "H", "D"); // Time Complexity: O(2^n)
+//        towerOfHanoi(n, "S", "H", "D"); // Time Complexity: O(2^n)
 
-        String name = "pratham";
-        printReverse(name, name.length()-1); // Time Complexity: O(n)
+        String str = "pratham";
+//        printReverse(str, str.length()-1); // Time Complexity: O(n)
 
-        String str = "aoghaoihgoagagah";
-        findOccurance(str, 0,'h');
-
+        str = "aoghaoihgoagagah";
+//      findOccurance(str, 0,'h');
         int[] arr = {1,3,4,6,8,9,0};
-        System.out.println(isSorted(arr,0));
+//        System.out.println(isSorted(arr,0));
+
+        str = "axbcxxdexfxgxxxhxi";
+//        moveAllX(str, 0, 0, ""); // Time Complexity: O(n)
+
+        str = "aabbcddefg";
+//        removeDuplicate(str, 0, "");
+
+        str = "abc";
+//        subsequences(str, 0,""); // Time Complexity: O(2^n)
+
+        str = "aaabc";
+        HashSet <String> set = new HashSet<>();
+//        uniqueSubsequences(str, 0,"", set);
+
+        str = "236";
+        printCombinations(str, 0, "");
     }
 }

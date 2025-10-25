b8c731b162c9e1b4a718edd796d86200f4e99ecd
Pratham Rathod <prathod2602005@gmail.com>
2025-10-25 10:00:51 +0530

    Backtracking and N-queens problem

---
+++ src/Concepts/Backtracking.java
@@ -0,0 +1,20 @@
+package Concepts;
+
+public class Backtracking {
+    public static void printPermutation(String str, String perm, int index){
+        if(str.isEmpty()){
+            System.out.println(perm);
+            return;
+        }
+        for(int i=0; i<str.length();i++){
+            char currChar = str.charAt(i);
+            String newStr = str.substring(0,i) + str.substring(i+1);
+            printPermutation(newStr, perm + currChar, index+1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String str = "ABC";
+        printPermutation(str, "", 0);
+    }
+}

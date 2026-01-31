a8726f416b8c6c3f433f7e5a3986c00c2c413765
Pratham Rathod <prathod2602005@gmail.com>
2026-02-01 00:11:43 +0530

    Striver Easy Array Problems

--- src/Arrays/ArrayConcept.java
+++ src/Arrays/ArrayConcept.java
@@ -50,7 +50,7 @@ public class ArrayConcept {
 
 //        int[] nums = {5,7,7,8,8,10};
 //        int[] range = searchRange(nums,8);
-//        System.out.println(Arrays.toString(range));
+//        System.out.println(EasyArray.toString(range));
 
 //        int[] nums = {1,3,5,6,7};
 //        int a = searchInput(nums, 8);
---
+++ src/StriverDSA/RunArray.java
@@ -0,0 +1,43 @@
+package StriverDSA;
+
+import java.util.Arrays;
+
+import static StriverDSA.Easy.EasyArray.*;
+
+public class RunArray {
+    public static void main(String[] args) {
+
+//        int[] arr = {2,5,1,3,0};
+//        System.out.println(findLargest(arr));
+
+//        int[] arr = {2,5,1,3,0};
+//        findSecond(arr);
+
+//        int[] arr = {1,2,3,4,5};
+//        int[] arr = {2,5,1,3,0};
+//        System.out.println(isSorted(arr));
+
+//        int[] arr = {1,1,1,2,2,3,3,3,3,4,4};
+//        System.out.println(Arrays.toString(removeDuplicate(arr)));
+
+//        int[] arr = {1,2,3,4,5};
+//        leftRotate(arr);
+//        rightRotate(arr);
+//        System.out.println(Arrays.toString(arr));
+
+//        RotateBy(arr,4,'r');
+//        System.out.println(Arrays.toString(arr));
+
+//        int[] arr = {0, 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1};
+//        moveZeros(arr);
+//        System.out.println(Arrays.toString(arr));
+
+//        int[] arr = {1,3,4,2,7,6,8};
+//        System.out.println(linearSearch(arr,4));
+
+//        int[] arr = {1,4,5,6,8,9};
+//        System.out.println(binarySearch(arr,6));
+
+    }
+
+}
